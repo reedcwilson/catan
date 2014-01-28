81e31571af921d43c47827f47869db80fe8ee38c
Jake Parkinson <parkinson9@gmail.com>
2014-01-27 20:28:56 -0700

    proxy updated

--- gameplay/js/model/proxy.js
+++ gameplay/js/model/proxy.js
@@ -139,6 +139,168 @@ method. 
 		@method changeLogLevel
 	*/
      Proxy.prototype.changeLogLevel = function() {};
+     /**
+		Sends a chat message
+		<pre>
+		PRE: Cookies must be set
+		PRE: 
+		POST: sends a chat to player by given id
+		</pre>
+		@method sendChat
+		@return Return's the 'Client Model' JSON (identical to game/model)
+	*/
+      Proxy.prototype.sendChat = function() {};
+      /**
+		Used to roll a number at the beginning of your turn
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		POST: gives the number rolled
+		</pre>
+		@method rollNumber
+	*/
+       Proxy.prototype.rollNumber = function() {};
+         /**
+		Used to finish a players turn
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		POST: the players turn is over
+		</pre>
+		@method finishTurn
+		@return Return's the 'Client Model' JSON (identical to game/model)
+	*/
+        Proxy.prototype.finishTurn = function() {};
+          /**
+		Buys a development card for yourself.
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: must have the correct resource cards
+		POST: player gets a devCard
+		</pre>
+		@method buyDevCard
+	*/
+         Proxy.prototype.buyDevCard = function() {};
+              /**
+		Plays a 'Year of Plenty' card from your hand to gain the two specified resources
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the year of plenty card
+		POST: year of plenty card is played
+		</pre>
+		@method playYearPlenty
+	*/
+          Proxy.prototype.playYearPlenty = function() {};
+                  /**
+	Plays a 'Road Building' card from your hand to build at the two spots specified
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the road building card
+		POST: road building card is played
+		</pre>
+		@method playRoadBuilding
+	*/
+           Proxy.prototype.playRoadBuilding = function() {};
+                  /**
+	Plays a 'Soldier' from your hand, selecting the new robber position and player to rob
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the soldier card
+		POST: soldier card is played
+		</pre>
+		@method playSoldier
+	*/
+	 Proxy.prototype.playSoldier = function() {};
+	  /**
+	Plays a 'Monopoly' card from you hand to monopolize the requested resource.
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the monopoly card
+		POST: monopoly card played
+		</pre>
+		@method playMonopoly
+	*/
+            Proxy.prototype.playMonopoly = function() {};
+             /**
+	Plays a 'Monument' card from your hand to give you a point
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the monument card
+		POST: player given a point
+		</pre>
+		@method playMonument
+	*/
+             Proxy.prototype.playMonument = function() {};
+                  /**
+	Builds a road for the specified player at the specified spot. Set true to free if it's during the setup
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the resources available
+		POST: road built
+		</pre>
+		@method buildRoad
+	*/
+              Proxy.prototype.buildRoad = function() {};
+                  /**
+	Builds a settlement for the specified player at the specified spot. Set true to free if it's during the setup
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the resources available
+		POST: settlement built
+		</pre>
+		@method buildSettlement
+	*/
+               Proxy.prototype.buildSettlement = function() {};
+                      /**
+	Builds a city for the specified player at the specified spot. Set true to free if it's during the setup.
+		<pre>
+		PRE: Cookies must be set
+		PRE: must be the players turn
+		PRE: player must have the resources available
+		POST: city built
+		</pre>
+		@method buildCity
+	*/
+                Proxy.prototype.buildCity = function() {};
+                            /**
+		Offers a trade to another player
+		<pre>
+		PRE: Cookies must be set
+		PRE: player must have the resources
+		POST: trade extended to another player
+		</pre>
+		@method offerTrade
+	*/
+                 Proxy.prototype.offerTrade = function() {};
+                           /**
+		Accept or reject a trade offered to you
+		<pre>
+		PRE: Cookies must be set
+		PRE: player must have the resources
+		POST: trade offer is closed
+		</pre>
+		@method acceptTrade
+	*/
+                  Proxy.prototype.acceptTrade = function() {};
+                               /**
+		Discards selected cards because of a 7 rolled
+		<pre>
+		PRE: Cookies must be set
+		PRE: player must have > 7 cards
+		POST: player has 7 cards left
+		</pre>
+		@method discardCards
+	*/
+                   Proxy.prototype.discardCards = function() {};
+
 
      return Proxy;
 })();
