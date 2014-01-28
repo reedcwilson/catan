c0c810b430ffcd50072e73e3e49861707d29c404
Reed Wilson <reedcwilson@gmail.com>
2014-01-27 21:00:57 -0700

    add YUIdoc for devCard, messageLine, messageList and turnTracker

---
+++ gameplay/js/model/messageLine.js
@@ -0,0 +1,55 @@
+
+
+catan = catan || {};
+catan.models = catan.models || {};
+
+catan.models.MessageLine = (function() {
+
+  /**
+    The MessageLine class is an object containing a single message
+
+    <pre>
+      Invariants:
+        INVARIANT: the message should never change after it is instantiated
+        INVARIANT: the source should never change after it is instantiated
+
+      PRE: message is a string
+      PRE: source is a string
+      POST: message is now set
+      POST: source is now set
+    </pre>
+
+    @class MessageLine
+    @constructor
+    @param {String} the message
+    @param {String} the source of the message
+    */
+  function MessageLine(message, source) { };
+
+  /**
+    Returns the message
+
+    <pre>
+      PRE: none
+      POST: none
+    </pre>
+
+    @method getMessage
+    @return {String}
+   */
+  MessageLine.prototype.getMessage = function() { };
+
+  /**
+    Returns the source
+
+    <pre>
+      PRE: none
+      POST: none
+    </pre>
+
+    @method getSource
+    @return {String}
+   */
+  MessageLine.prototype.getSource = function() { };
+
+});
