3873acca065a23f02671ce4b6dcbcfcc4ab3fb9d
Reed Wilson <reedcwilson@gmail.com>
2014-03-29 23:31:05 -0600

    modify test cases

--- test/src/datamodel/hexgrid/HexGridTests.java
+++ test/src/datamodel/hexgrid/HexGridTests.java
@@ -2,12 +2,19 @@ package src.datamodel.hexgrid;
 
 import com.catan.main.datamodel.game.CreateGameRequest;
 import com.catan.main.datamodel.game.Game;
+import com.catan.main.datamodel.hexgrid.HexGrid;
+import com.catan.main.datamodel.hexgrid.base.Location;
 import com.catan.main.datamodel.hexgrid.edge.Edge;
 import com.catan.main.datamodel.hexgrid.edge.EdgeDirection;
 import com.catan.main.datamodel.hexgrid.edge.EdgeLocation;
+import com.catan.main.datamodel.hexgrid.vertex.VertexDirection;
+import com.catan.main.datamodel.hexgrid.vertex.VertexLocation;
 import com.catan.main.server.Client;
 import com.catan.main.server.ServerUtils;
 import org.junit.*;
+
+import java.util.Collection;
+
 import static org.junit.Assert.*;
 
 public class HexGridTests {
@@ -28,11 +35,23 @@ public class HexGridTests {
     }
 
     @Test
-    public void testRandomTestName() {
+    public void hexGridTests() {
         try {
             EdgeLocation loc = new EdgeLocation(0, 0, EdgeDirection.NE);
-            Edge edge = _game.getModel().getMap().getHexGrid().getEdge(loc);
+            HexGrid grid = _game.getModel().getMap().getHexGrid();
+            Edge edge = grid.getEdge(loc);
             assertSame(edge.getLocation(), null);
+            assertTrue(grid.getOffsets() != null);
+            int radius = grid.getRadius();
+            assertTrue(grid.getY0() > -radius && grid.getY0() < radius && grid.getX0() > -radius && grid.getX0() < radius);
+//            assertTrue(grid.getLocations() != null && !grid.getLocations().isEmpty());
+            System.out.println(grid.toString());
+            int code = grid.hashCode();
+            assertTrue(code != 0 || code != 1);
+            assertTrue(!grid.equals(grid));
+            assertTrue(grid.getVertex(new VertexLocation(0,0, VertexDirection.NW)) != null);
+            assertTrue(grid.getRadius() > 0);
+            assertTrue(grid.getEdge(new EdgeLocation(0, 0, EdgeDirection.NW)) != null);
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
